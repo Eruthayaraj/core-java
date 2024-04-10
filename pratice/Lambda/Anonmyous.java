08cca996766f2a2a72cde1054146e279ec41ec88
Eruthayaraj <163977723+Eruthayaraj@users.noreply.github.com>
2024-04-10 15:19:53 +0530

    pratice

---
+++ pratice/Lambda/Anonmyous.java
@@ -0,0 +1,27 @@
+package Lambda;
+
+public class Anonmyous {
+    public static void main(String[] args) {
+     Emp obj=new Emp(){
+      public void display()
+     {
+      System.out.println("hello");
+     }
+     public void display1()
+     {
+       System.out.println("hii");
+     }
+     }  ; 
+     obj.display();
+    }
+}
+
+class Emp{
+    String name="hh";
+    String Salary;
+  public void display()
+  {
+    System.out.println("hii");
+  }
+ 
+}
